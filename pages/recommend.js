e2414878ae1e7680db5c095738e7fbfe1ec3c21e
Angel <angelCL0@outlook.com>
2020-09-12 18:25:45 -0400

    Awesome

--- pages/recommend.js
+++ pages/recommend.js
@@ -1,9 +1,15 @@
 import ContactForm from "../components/form";
 import styles from "../components/form.module.css";
+import Link from "next/link";
 
 export default function recommend() {
   return (
     <div className={styles.reco_container}>
+      <Link href="/">
+        <a>
+          <p className={styles.volver}>← Volver</p>
+        </a>
+      </Link>
       <div className={styles.reco_group}>
         <h1>¿Le gustaría recomendar un libro?</h1>
         <ContactForm />
