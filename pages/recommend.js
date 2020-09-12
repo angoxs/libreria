ff46dfbd502ab2d5615570ee68723d3c0a386402
Angel <angelCL0@outlook.com>
2020-09-12 18:17:20 -0400

    Awesome

--- pages/recommend.js
+++ pages/recommend.js
@@ -5,7 +5,7 @@ export default function recommend() {
   return (
     <div className={styles.reco_container}>
       <div className={styles.reco_group}>
-      <h1>Deseas recomendar un libro?</h1>
+        <h1>¿Le gustaría recomendar un libro?</h1>
         <ContactForm />
       </div>
     </div>
